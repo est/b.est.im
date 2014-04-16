661749dc761b59ba3e1a8554725378ff46158ca1
est <electronicstar@126.com>
2014-04-16 16:51:18 +0800

    bottle serverion

---
+++ src/urls.py
@@ -0,0 +1,5 @@
+routes = (
+    # path, function, http_method, name                                                                                                                                   
+    ('/', index_get, 'GET', 'index_get'),
+    ('/', index_post, 'POST', 'index_post'),
+)   
